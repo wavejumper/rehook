5ce78d96a779131e0dc06a87a0599e205c827031
Thomas Crowley <crowley@kibu.com.au>
2019-11-23 18:23:38 +1100

    2.0.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject rehook "2.0.0"
+(defproject rehook "2.0.1"
   :description "React Hooks for Clojurescript"
   :url "https://github.com/wavejumper/rehook"
 
   :dependencies
-  [[rehook/core "2.0.0"]
-   [rehook/dom "2.0.0"]
-   [rehook/test "2.0.0"]])
+  [[rehook/core "2.0.1"]
+   [rehook/dom "2.0.1"]
+   [rehook/test "2.0.1"]])
--- rehook-dom/project.clj
+++ rehook-dom/project.clj
@@ -1,4 +1,4 @@
-(defproject  rehook/dom "2.0.0"
+(defproject  rehook/dom "2.0.1"
   :description "React component DSL for Clojurescript"
   :url "https://github.com/wavejumper/rehook"
 
